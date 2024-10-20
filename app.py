134b958626623dbab36c9f1aff345c6728e5088a
ZULQAR NAIN <zulqarnainhumbly258@gmail.com>
2024-10-20 23:40:54 +0500

    new changes

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ async def homepage():
         html = f.read()
     return HTMLResponse(html)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=3000)
